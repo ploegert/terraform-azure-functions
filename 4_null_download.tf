d75251fc95d3a1cf8ad12a1d98e2b22fdfef4c95
praveenkumar1290 <43928240+praveenkumar1290@users.noreply.github.com>
2021-03-01 17:40:20 +0530

    Update 4_null_download.tf
    Jfrog commands session added

--- 4_null_download.tf
+++ 4_null_download.tf
@@ -1,12 +1,16 @@
 #+=======================================================================================
 # Download dataflowapi function
+azure_command = "az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}"
+jfrog_command = "jfrog rt download  --flat=true"
+download_command = var.is_azure_artifact ? azure_command : jfrog_command 
 resource "null_resource" "download" {
   triggers = {
     version = local.file_version_name
   }
   provisioner "local-exec" {
-    command = "az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}"
-  }
+    
+	command = local.download_command
+	}
  
   depends_on = [
     azurerm_function_app.fnapp,

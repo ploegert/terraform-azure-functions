6b8c7cf25cc5082f7f8e8d5661b9f6fa16db7ff7
Justin Ploegert <cploegj@jci.com>
2021-04-08 13:27:37 -0500

    added an explict login

--- 5_null_deploy.tf
+++ 5_null_deploy.tf
@@ -12,13 +12,17 @@ resource "time_sleep" "wait_x_seconds_after_creation" {
 }
 
 #+=======================================================================================
-# Deploy dataflowapi function
+# Deploy function
 resource "null_resource" "deploy" {
   triggers = {
     version = local.file_version_name
   }
   provisioner "local-exec" {
-    command = "az functionapp deployment source config-zip -g ${var.common_vars.resource_group_name} -n ${var.functionapp.fn_name} --src ./${local.file_version_name}"
+    command = <<EOT
+      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id}
+      az account set --subscription ${var.deployment.subscription_id}
+      az functionapp deployment source config-zip -g ${var.common_vars.resource_group_name} -n ${var.functionapp.fn_name} --src ./${local.file_version_name}
+    EOT
   }
  
   depends_on = [

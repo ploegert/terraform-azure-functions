7410afaa2851eb6365c8379afe1544caf87529ef
Justin Ploegert <cploegj@jci.com>
2021-04-08 13:37:59 -0500

    chaining steps together in a single step

--- 5_null_deploy.tf
+++ 5_null_deploy.tf
@@ -19,9 +19,11 @@ resource "null_resource" "deploy" {
   }
   provisioner "local-exec" {
     command = <<EOT
-      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id}
-      az account set --subscription ${var.deployment.subscription_id}
+      az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}
+      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output json
+      az account set --subscription ${var.deployment.subscription_id} --output json
       az functionapp deployment source config-zip -g ${var.common_vars.resource_group_name} -n ${var.functionapp.fn_name} --src ./${local.file_version_name}
+      az functionapp config appsettings delete -g ${var.common_vars.resource_group_name} -n  ${var.functionapp.fn_name} --setting-names AzureWebJobsDashboard
     EOT
   }
  

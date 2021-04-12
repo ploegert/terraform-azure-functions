7f2577621408f3824aa3f89034fa62e921773b53
Sai Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-04-12 17:09:28 -0500

    Update 6_null_vnet_config.tf

--- 6_null_vnet_config.tf
+++ 6_null_vnet_config.tf
@@ -19,8 +19,8 @@ resource "null_resource" "vnet_config" {
 
   provisioner "local-exec" {
     command = <<EOT
-      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output none
-      az account set --subscription ${var.deployment.subscription_id} --output none
+      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
+      az account set --subscription ${var.deployment.subscription_id} --output tsv
       az functionapp vnet-integration add -g ${var.common_vars.resource_group_name} -n  ${var.functionapp.fn_name} --vnet ${var.function_vnet.vnet_id} --subnet ${var.function_vnet.subnet_name}
     EOT
   }

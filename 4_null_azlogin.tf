e519489560dd7974e5c7e005531b916eb8befdf4
Sai Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-04-12 17:10:03 -0500

    Update 4_null_azlogin.tf

--- 4_null_azlogin.tf
+++ 4_null_azlogin.tf
@@ -7,8 +7,8 @@ resource "null_resource" "az_login" {
   
   provisioner "local-exec" {
     command = <<EOT
-      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output none
-      az account set --subscription ${var.deployment.subscription_id} --output none
+      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
+      az account set --subscription ${var.deployment.subscription_id} --output tsv
     EOT
   }
 

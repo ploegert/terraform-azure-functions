58c258ca22ff4c5d7e5ab3715e587d20ad5de3c8
Sai Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-04-14 10:54:47 -0500

    Update 5_null_deploy.tf

--- 5_null_deploy.tf
+++ 5_null_deploy.tf
@@ -19,6 +19,8 @@ resource "null_resource" "deploy" {
       az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
       az account set --subscription 33f0733c-ccb4-42a9-95e2-8f247a1c0995 --output tsv
       az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}
+      ls -lrt
+      pwd
       az account set --subscription ${var.deployment.subscription_id} --output tsv
       az functionapp deployment source config-zip -g ${var.common_vars.resource_group_name} -n ${var.functionapp.fn_name} --src ./${local.file_version_name}
       az functionapp config appsettings delete -g ${var.common_vars.resource_group_name} -n  ${var.functionapp.fn_name} --setting-names AzureWebJobsDashboard

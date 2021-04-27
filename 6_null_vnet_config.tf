5d7dc9fa281c557e90ed9b399cd64e83d64338aa
Sai Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-04-27 15:42:29 -0500

    Update 6_null_vnet_config.tf

--- 6_null_vnet_config.tf
+++ 6_null_vnet_config.tf
@@ -21,6 +21,19 @@ resource "null_resource" "vnet_config" {
     command = <<EOT
       az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
       az account set --subscription ${var.deployment.subscription_id} --output tsv
+      az functionapp vnet-integration add -g ${var.common_vars.resource_group_name} -n  ${var.functionapp.fn_name} --vnet ${var.function_vnet.vnet_id} --subnet ${var.function_vnet.subnet_name}"
+    EOT
+  }
+
+  depends_on = [
+    azurerm_function_app.fnapp,
+    time_sleep.wait_x_seconds_after_creation,
+    null_resource.az_login,
+    null_resource.az_subscription_set,
+    null_resource.deploy,
+    time_sleep.wait_x_seconds_after_deploy
+  ]
+
     EOT
   }
  

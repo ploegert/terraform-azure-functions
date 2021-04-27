cb3e89fa0bdf9038f7697f4718eb083d4af68c8c
Sai Anaparthi <47329009+saisasank@users.noreply.github.com>
2021-04-27 15:40:59 -0500

    Update 5_null_deploy.tf

--- 5_null_deploy.tf
+++ 5_null_deploy.tf
@@ -15,12 +15,11 @@ resource "null_resource" "deploy" {
     version = local.file_version_name
   }
   provisioner "local-exec" {
-    command = <<EOT
+    command = <<EOT      
       az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
-      az account set --subscription 33f0733c-ccb4-42a9-95e2-8f247a1c0995 --output tsv
-      az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}
-      ls -lrt
-      pwd
+      az account set --subscription ${var.deployment.subscription_id} --output tsv
+      az storage blob download --file ./${local.file_version_name} --name ${local.file_version_name} --subscription ${var.artifacts.subscription} --container-name ${var.artifacts.container_name} --account-name ${var.artifacts.storage_account}  --account-key ${var.artifacts.account_key}
+      az login --service-principal -u ${var.deployment.client_id} -p ${var.deployment.client_secret} --tenant ${var.deployment.tenant_id} --output tsv
       az account set --subscription ${var.deployment.subscription_id} --output tsv
       az functionapp deployment source config-zip -g ${var.common_vars.resource_group_name} -n ${var.functionapp.fn_name} --src ./${local.file_version_name}
       az functionapp config appsettings delete -g ${var.common_vars.resource_group_name} -n  ${var.functionapp.fn_name} --setting-names AzureWebJobsDashboard
